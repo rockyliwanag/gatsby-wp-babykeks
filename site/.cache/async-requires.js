4f29da10ddd9bd7d8ed9da59d3872fbc33e8f9fc
Rocky Liwanag <studiorockprod@me.com>
2020-08-17 17:54:40 -0700

    404 page

--- site/.cache/async-requires.js
+++ site/.cache/async-requires.js
@@ -3,6 +3,7 @@ const preferDefault = m => m && m.default || m
 
 exports.components = {
   "component---cache-dev-404-page-js": () => import("./../../.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---packages-gatsby-wordpress-theme-babykeks-src-pages-404-js": () => import("./../../../packages/gatsby-wordpress-theme-babykeks/src/pages/404.js" /* webpackChunkName: "component---packages-gatsby-wordpress-theme-babykeks-src-pages-404-js" */),
   "component---packages-gatsby-wordpress-theme-babykeks-src-templates-front-page-index-js": () => import("./../../../packages/gatsby-wordpress-theme-babykeks/src/templates/front-page/index.js" /* webpackChunkName: "component---packages-gatsby-wordpress-theme-babykeks-src-templates-front-page-index-js" */),
   "component---packages-gatsby-wordpress-theme-babykeks-src-templates-page-index-js": () => import("./../../../packages/gatsby-wordpress-theme-babykeks/src/templates/page/index.js" /* webpackChunkName: "component---packages-gatsby-wordpress-theme-babykeks-src-templates-page-index-js" */)
 }
